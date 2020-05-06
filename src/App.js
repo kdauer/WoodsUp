44b742f2400705200feefe463d174aad9af827dc
Konstantin <dauer.konstantin@gmail.com>
2020-05-06 17:43:24 +0200

    created nav hamburger

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import About from "./components/About";
 import PProjects from "./components/pprojects";
 import FProjects from "./components/fprojects";
 import Support from "./components/support";
-import Links from "./components/links";
-import { Switch, Route } from "react-router-dom";
+import Knowing from "./components/links.js";
+import { Switch, Route, Link } from "react-router-dom";
 import Footer from "./components/Footer";
 // import Impress from "./components/legalnotice";
 // import DSGVO from "./components/privacypolicy";
@@ -16,19 +16,21 @@ function App() {
   return (
     <div className="App">
       <div className="nav-img">
-        <h1 className="logo" style={{ color: "white" }}>
-          WoodsUp
+        <h1 className="logo">
+          <Link to="/" style={{ color: "white", textDecoration: "none" }}>
+            WoodsUp
+          </Link>
         </h1>
 
         <NavBar />
       </div>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
+        <Route exact path="/about" component={About} />
         <Route exact path="/past-projects" component={PProjects} />
         <Route exact path="/future-projects" component={FProjects} />
-        <Route path="/support" component={Support} />
-        <Route path="/links" component={Links} />
+        <Route exact path="/support" component={Support} />
+        <Route exact path="/links" component={Knowing} />
         {/* <Route path="/legalnotice" component={Impress} />
         <Route path="/privacypolicy" component={DSGVO} /> */}
       </Switch>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,28 +5,34 @@ export default function NavBar() {
   return (
     <div>
       <nav className="nav">
-        <ul>
-          <li>
-            {" "}
-            <Link to="/about">Über uns</Link>
-          </li>
-          <li>
-            {" "}
-            <Link to="/past-projects">vergangene Projekte</Link>
-          </li>
-          <li>
-            {" "}
-            <Link to="/future-projects">zukünftige Projekte</Link>
-          </li>
-          <li>
-            {" "}
-            <Link to="/support">Unterstützung</Link>
-          </li>
-          <li>
-            {" "}
-            <Link to="/links">Wissenswertes</Link>
-          </li>
-        </ul>
+        <div className="ham">
+          <label htmlFor="toggle">&#9776;</label>
+          <input type="checkbox" id="toggle" />
+          <div className="menu">
+            <ul>
+              <li>
+                {" "}
+                <Link to="/about">Über uns</Link>
+              </li>
+              <li>
+                {" "}
+                <Link to="/past-projects">vergangene Projekte</Link>
+              </li>
+              <li>
+                {" "}
+                <Link to="/future-projects">zukünftige Projekte</Link>
+              </li>
+              <li>
+                {" "}
+                <Link to="/support">Unterstützung</Link>
+              </li>
+              <li>
+                {" "}
+                <Link to="/links">Wissenswertes</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
       </nav>
     </div>
   );

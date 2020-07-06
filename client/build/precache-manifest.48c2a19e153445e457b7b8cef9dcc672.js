d63e3a8d85f5177ee6a5dea3524a8108311bfbb4
Konstantin <dauer.konstantin@gmail.com>
2020-07-06 10:52:12 +0200

    run build

---
+++ client/build/precache-manifest.48c2a19e153445e457b7b8cef9dcc672.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39d59a3eefaf50eb7edba1e80ed3a684",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e46b8757d5d486adb52a",
+    "url": "/static/css/main.79130755.chunk.css"
+  },
+  {
+    "revision": "6d55853a1b72131052d7",
+    "url": "/static/js/2.17505fed.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.17505fed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e46b8757d5d486adb52a",
+    "url": "/static/js/main.d3aa121c.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  },
+  {
+    "revision": "d328940a97c0c60009ca5f60247274ea",
+    "url": "/static/media/IMG_2675.d328940a.JPG"
+  },
+  {
+    "revision": "824523fa59b02f30fca2451d3333607e",
+    "url": "/static/media/paul-gilmore-KT3WlrL_bsg-unsplash.824523fa.jpg"
+  }
+]);

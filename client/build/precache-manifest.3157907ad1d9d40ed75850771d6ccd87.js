22e3d5a4041ad15551ff56160cc713b51a604b8d
Konstantin <dauer.konstantin@gmail.com>
2020-08-16 11:53:43 +0200

    implemented cookie

--- client/build/precache-manifest.3157907ad1d9d40ed75850771d6ccd87.js
+++ client/build/precache-manifest.3157907ad1d9d40ed75850771d6ccd87.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "df81e57c7f93b4915610c6f7f469850a",
+    "revision": "60be52cbf5cc25c72bee200ede41c7ba",
     "url": "/index.html"
   },
   {
-    "revision": "4e1290300263b9ad7290",
+    "revision": "af38b94dd43b43c5bc1e",
     "url": "/static/css/main.08e5a5e7.chunk.css"
   },
   {
-    "revision": "dbfa6122d0328ca0712d",
-    "url": "/static/js/2.821b797a.chunk.js"
+    "revision": "f276bf62cd5f5ddc48be",
+    "url": "/static/js/2.d4afb815.chunk.js"
   },
   {
-    "revision": "928d7b5eb39d16fe9a880722c974b51e",
-    "url": "/static/js/2.821b797a.chunk.js.LICENSE.txt"
+    "revision": "db5090193006a19a1130ddb69cea31f3",
+    "url": "/static/js/2.d4afb815.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4e1290300263b9ad7290",
-    "url": "/static/js/main.6c91d679.chunk.js"
+    "revision": "af38b94dd43b43c5bc1e",
+    "url": "/static/js/main.5296e5e9.chunk.js"
   },
   {
     "revision": "cda345fb50b7e11c0025",
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.14173fc5e9c2ca074e55558cf7d75b82.js"
+  "/precache-manifest.3157907ad1d9d40ed75850771d6ccd87.js"
 );
 
 self.addEventListener('message', (event) => {
